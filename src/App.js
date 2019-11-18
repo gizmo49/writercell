e9226d4162b3a48a47029d273dc30fd14ea222fe
gizmo49 <chikacheezy@gmail.com>
2019-11-18 16:02:58 +0100

    feat: simple routing system set

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 import "./assets/css/main.css";
 import "./assets/css/fonts.css";
 
-class App extends Component {
+const hist = createBrowserHistory();
+
+const Home = () => (<h6>home</h6>);
+const NotFound = () => (<h1 className="text-center">404 2</h1>);
+const LoginPage = () => (<h6>Login</h6>);
+
+export default class App extends Component {
   
   render = () => {
     return (
-      <div>
-        <p> the app </p>
-      </div>
+      <>
+        <Router history={hist}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="*" render={() => <NotFound />} />
+          </Switch>
+        </Router>
+      </>
     );
   }
-}
 
-export default App;
+}
